efdd732d16b6399dc609c8eab1b72489ae59d895
Ando Roots <ando.roots@bigbank.ee>
2015-10-27 11:26:19 +0200

    API-1510 Add authentication status constants

--- src/Request/GetMobileAuthenticateStatus.php
+++ src/Request/GetMobileAuthenticateStatus.php
@@ -7,6 +7,58 @@ namespace Bigbank\MobileId\Request;
 class GetMobileAuthenticateStatus extends AbstractRequest
 {
 
+    /**
+     * Authentication is still on the way, ask again later
+     */
+    const OUTSTANDING_TRANSACTION = 'OUTSTANDING_TRANSACTION';
+
+    /**
+     * Authentication successful
+     */
+    const USER_AUTHENTICATED = 'USER_AUTHENTICATED';
+
+    /**
+     * The action is completed but the signature created is not valid
+     */
+    const NOT_VALID = 'NOT_VALID';
+
+    /**
+     * Authentication timed out
+     */
+    const EXPIRED_TRANSACTION = 'EXPIRED_TRANSACTION';
+
+    /**
+     * The user cancelled
+     */
+    const USER_CANCEL = 'USER_CANCEL';
+
+    /**
+     * Mobile ID SIM card is not yet ready
+     */
+    const MID_NOT_READY = 'MID_NOT_READY';
+
+    /**
+     * The phone is switched off or out of service coverage
+     */
+    const PHONE_ABSENT = 'PHONE_ABSENT';
+
+    /**
+     * General error when sending the message
+     *
+     * (the phone is incapable of receiving the
+     * message, error in messaging server etc.)
+     */
+    const SENDING_ERROR = 'SENDING_ERROR';
+
+    /**
+     * SIM application error
+     */
+    const SIM_ERROR = 'SIM_ERROR';
+
+    /**
+     * Error in the SOAP API
+     */
+    const INTERNAL_ERROR = 'INTERNAL_ERROR';
 
     /**
      * {@inheritdoc}
